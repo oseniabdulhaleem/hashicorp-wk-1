2a71ef02a0854f3471c9de93f59d7560fb1cf8e8
oseni abdulhaleem <oseniabdulhaleem@gmail.com>
2025-08-20 11:14:50 +0100

    removed old file

--- site/config.js
+++
@@ -1,6 +0,0 @@
-window.CONFIG = {
-  personalName: "Abdulhaleem Oseni",
-  githubUsername: "oseniabdulhaleem",
-  linkedinUrl: "https://linkedin.com/in/oseniabdulhaleem",
-  twitterUsername: "adel_wale",
-};
